22c51841a990004e2de16d33e5a2d76e6058971a
hujunxianligong <hujunxianligong@gmail.com>
2020-01-22 16:48:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # TensorFlow-TextGCN
 Efficient implementation of "Graph Convolutional Networks for Text Classification"
+
+## Requirements
+
++ Python >= 3.5
++ tqdm: `pip install tqdm`
++ TensorFlow >=1.14.0 or >=2.0.0
++ tf_geometric:
+Use one of the following commands below to install tf_geometric (and tensorflow):
+```bash
+pip install -U tf_geometric # this will not install the tensorflow/tensorflow-gpu package
+
+pip install -U tf_geometric[tf1-cpu] # this will install TensorFlow 1.x CPU version
+
+pip install -U tf_geometric[tf1-gpu] # this will install TensorFlow 1.x GPU version
+
+pip install -U tf_geometric[tf2-cpu] # this will install TensorFlow 2.x CPU version
+
+pip install -U tf_geometric[tf2-gpu] # this will install TensorFlow 2.x GPU version
+```
+
+
---
+++ train_text_gcn.py
@@ -0,0 +1,166 @@
+# coding=utf-8
+import os
+import numpy as np
+from collections import Counter
+
+from tqdm import tqdm
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow import keras
+import tensorflow as tf
+import tf_geometric as tfg
+import pickle
+
+data_dir = "datasets/rt-polarity"
+
+texts = []
+labels = []
+for fname, label in [["rt-polarity.pos", 1], ["rt-polarity.neg", 0]]:
+    fpath = os.path.join(data_dir, fname)
+    with open(fpath, "r", encoding="utf-8") as f:
+        for line in f:
+            texts.append(line.strip())
+            labels.append(label)
+
+train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, test_size=0.2)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+
+train_sequences = tokenizer.texts_to_sequences(train_texts)
+test_sequences = tokenizer.texts_to_sequences(test_texts)
+
+class PMIModel(object):
+
+    def __init__(self):
+        self.word_counter = None
+        self.pair_counter = None
+
+    def get_pair_id(self, word0, word1):
+        pair_id = tuple(sorted([word0, word1]))
+        return pair_id
+
+    def fit(self, sequences, window_size):
+
+        self.word_counter = Counter()
+        self.pair_counter = Counter()
+        num_windows = 0
+        for sequence in tqdm(sequences):
+            for offset in range(len(sequence) - window_size):
+                window = sequence[offset:offset + window_size]
+                num_windows += 1
+                for i, word0 in enumerate(window):
+                    self.word_counter[word0] += 1
+                    for j, word1 in enumerate(window[i+1:]):
+                        pair_id = self.get_pair_id(word0, word1)
+                        self.pair_counter[pair_id] += 1
+
+        for word, count in self.word_counter.items():
+            self.word_counter[word] = count / num_windows
+        for pair_id, count in self.pair_counter.items():
+            self.pair_counter[pair_id] = count / num_windows
+
+    def transform(self, word0, word1):
+        a = self.word_counter[word0]
+        b = self.word_counter[word1]
+        pair_id = self.get_pair_id(word0, word1)
+        c = self.pair_counter[pair_id]
+
+        if a == 0 or b == 0 or c == 0:
+            return 0
+
+        pmi = np.log(a / (b * c))
+        pmi = np.maximum(pmi, 0.0)
+        return pmi
+
+
+def build_word_graph(num_words, pmi_model, embedding_size):
+    x = tf.Variable(tf.random.truncated_normal([num_words, embedding_size], stddev=1 / np.sqrt(embedding_size)), dtype=tf.float32)
+    edges = []
+    edge_weight = []
+    for (word0, word1) in pmi_model.pair_counter.keys():
+        pmi = pmi_model.transform(word0, word1)
+        if pmi > 0:
+            edges.append([word0, word1])
+            edge_weight.append(pmi)
+            edges.append([word1, word0])
+            edge_weight.append(pmi)
+    edge_index = np.array(edges).T
+    return tfg.Graph(x=x, edge_index=edge_index, edge_weight=edge_weight)
+
+
+def build_combined_graph(word_graph, sequences, embedding_size):
+    num_words = word_graph.num_nodes
+    x = tf.zeros([len(sequences), embedding_size], dtype=tf.float32)
+    edges = []
+    edge_weight = []
+    for i, sequence in enumerate(sequences):
+        doc_node_index = num_words + i
+        for word in sequence:
+            edges.append([doc_node_index, word])  # only directed edge
+            edge_weight.append(1.0)  # use BOW instaead of TF-IDF
+    edge_index = np.array(edges).T
+    x = tf.concat([word_graph.x, x], axis=0)
+    edge_index = np.concatenate([word_graph.edge_index, edge_index], axis=1)
+    edge_weight = np.concatenate([word_graph.edge_weight, edge_weight], axis=0)
+    return tfg.Graph(x=x, edge_index=edge_index, edge_weight=edge_weight)
+
+
+# building PMI model is time consuming, using cache to optimize
+pmi_cache_path = "cached_pmi_model.p"
+if os.path.exists(pmi_cache_path):
+    with open(pmi_cache_path, "rb") as f:
+        pmi_model = pickle.load(f)
+else:
+    pmi_model = PMIModel()
+    pmi_model.fit(train_sequences, window_size=10)
+    with open(pmi_cache_path, "wb") as f:
+        pickle.dump(pmi_model, f)
+
+
+
+embedding_size = 50
+num_words = len(tokenizer.word_index)
+word_graph = build_word_graph(num_words, pmi_model, embedding_size)
+train_combined_graph = build_combined_graph(word_graph, train_sequences, embedding_size)
+test_combined_graph = build_combined_graph(word_graph, test_sequences, embedding_size)
+
+print(word_graph)
+print(train_combined_graph)
+print(test_combined_graph)
+
+
+num_classes = 2
+gcn0 = tfg.layers.GCN(32, activation=tf.nn.relu)
+gcn1 = tfg.layers.GCN(num_classes)
+dropout = keras.layers.Dropout(0.2)
+
+
+def forward(graph, training=False):
+    h = gcn0([graph.x, graph.edge_index, graph.edge_weight], cache=graph.cache)
+    h = dropout(h, training=training)
+    h = gcn1([h, graph.edge_index, graph.edge_weight], cache=graph.cache)
+    return h
+
+
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
+for step in range(1000):
+    with tf.GradientTape() as tape:
+        logits = forward(train_combined_graph, training=True)[num_words:]
+        losses = tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits,
+            labels=tf.one_hot(train_labels, depth=num_classes)
+        )
+        mean_loss = tf.reduce_mean(losses)
+
+    vars = tape.watched_variables()
+    grads = tape.gradient(losses, vars)
+    optimizer.apply_gradients(zip(grads, vars))
+
+    if step % 10 == 0:
+        logits = forward(test_combined_graph)[num_words:]
+        preds = tf.argmax(logits, axis=-1)
+        corrects = tf.cast(tf.equal(preds, test_labels), tf.float32)
+        accuracy = tf.reduce_mean(corrects)
+        print("step = {}\tloss = {}\ttest_accuracy = {}".format(step, mean_loss, accuracy))
